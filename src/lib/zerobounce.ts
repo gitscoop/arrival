29c09296ee8b9489493c4212d3341274233c3da9
Nabarun <64539836+nabarvn@users.noreply.github.com>
2026-07-23 21:51:21 +0530

    Add ZbStatus and ZbSubStatus enums for ZeroBounce integration

---
+++ src/lib/zerobounce.ts
@@ -0,0 +1,11 @@
+export enum ZbStatus {
+  Valid = "valid",
+  Invalid = "invalid",
+  DoNotMail = "do_not_mail",
+}
+
+export enum ZbSubStatus {
+  PossibleTypo = "possible_typo",
+  RoleBased = "role_based",
+  Disposable = "disposable",
+}
