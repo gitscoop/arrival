832af15596c891c72924b6beb0b2b1f0414a4c4a
Nabarun <64539836+nabarvn@users.noreply.github.com>
2026-07-24 16:32:05 +0530

    Add interfaces for audio handling

---
+++ src/types/audio.d.ts
@@ -0,0 +1,13 @@
+export interface SoundAsset {
+  name: string;
+  dataUri: string;
+  duration: number;
+  format: "mp3" | "wav" | "ogg";
+  license: "CC0" | "OGA-BY" | "MIT";
+  author: string;
+}
+
+export interface PlaySoundOptions {
+  volume?: number;
+  playbackRate?: number;
+}
